8788eae68b3448f0d9619841d6d1f9db5bdfe7cd
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-27 14:33:05 +0200

    Added zombie structure

--- Contract.sol
+++ Contract.sol
@@ -5,4 +5,9 @@ pragma solidity ^0.8.0;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
