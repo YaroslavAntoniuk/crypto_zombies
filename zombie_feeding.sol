7f47ba454977135e2b5a0e3a185bf55afe3e7c02
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-27 16:44:23 +0200

    Added new function to the zombie feeding contract

--- zombie_feeding.sol
+++ zombie_feeding.sol
@@ -4,4 +4,12 @@ pragma solidity ^0.8.24;
 
 import "./zombie_factory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        _createZombie("NoName", newDna);
+    }
+}
