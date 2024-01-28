9f2253ed7585065bb940c5fe6f0fc02fe902bb62
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-28 12:05:18 +0200

    Added attack function

--- zombie_attack.sol
+++ zombie_attack.sol
@@ -4,6 +4,7 @@ import "./zombie_helper.sol";
 
 contract ZombieAttack is ZombieHelper {
     uint randNonce = 0;
+    uint attackVictoryProbability = 70;
 
     function randMod(uint _modulus) internal returns (uint) {
         randNonce++;
@@ -14,5 +15,22 @@ contract ZombieAttack is ZombieHelper {
                     abi.encodePacked(block.timestamp, msg.sender, randNonce)
                 )
             ) % _modulus;
+    }
+
+    function attack(uint _zombieId, uint _targetId) external {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
+
+        if (rand <= attackVictoryProbability) {
+            myZombie.level++;
+            myZombie.winCount++;
+            enemyZombie.lossCount++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
     }
 }
